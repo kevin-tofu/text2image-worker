fbd0fc9e7b77ba4e3afdd83c6f014340228073d2
kohei <koheitech001@gmail.com>
2023-04-29 08:51:25 +0900

    up

---
+++ src/diffuser.py
@@ -0,0 +1,40 @@
+# from typing import NamedTuple
+from src.config import Config
+from diffusers import StableDiffusionPipeline
+# import torch
+from src.logconf import mylogger
+logger = mylogger(__name__)
+
+class diffuser_host():
+
+    def __init__(self, cfg: Config):
+        super().__init__()
+        
+        logger.info(cfg.model_id)
+        logger.info(cfg.model_path)
+        logger.info(cfg.model_device)
+        # model_id = "runwayml/stable-diffusion-v1-5"
+        pipe = StableDiffusionPipeline.from_pretrained(
+            cfg.model_id,
+            # torch_dtype=torch.float16,
+            cache_dir=cfg.model_path
+        )
+        # self.pipe = pipe.to("cpu")
+        self.pipe = pipe.to(cfg.model_device)
+
+        # prompt = "a photo of an astronaut riding a horse on mars"
+        
+
+    
+    def get_image(
+        self,
+        **kwargs
+    ):
+        
+        prompt = kwargs['prompt']
+        image = self.pipe(prompt).images[0]
+        image.save(f"./temp/{prompt}.png")
+        print(type(image))
+
+        return image
+
